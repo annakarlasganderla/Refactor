c345d5777d5c6ffe48f6ffa1d112279b1e20de44
Vinícius Frasson <vinifrasson13@gmail.com>
2022-10-18 21:25:48 -0300

    Refactor

--- refactor.js
+++ refactor.js
@@ -1,27 +1,31 @@
 function returnRandomNumber() {
   return Math.random();
 }
+function round(num){
+  return `Seu número é ${Math.round(num * 100)}!`;
+}
 
+//Adicao
 function sumToRandomNumber(num) {
-  const numberToSum = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToSum * 100) + num}!`;
+  return round(returnRandomNumber()) + num;
 }
 
+//Subtracao
 function subtractRandomNumber(num) {
-  const numberToSubtract = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToSubtract * 100) - num}!`;
+  return round(returnRandomNumber()) - num;
 }
 
+//Multiplicacao
 function multiplyToRandomNumber(num) {
-  const numberToMultiply = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToMultiply * 100) * num}!`;
+  return round(returnRandomNumber()) * num;
 }
 
+//Divisao
 function divideRandomNumber(num) {
-  const numberToDivide = returnRandomNumber();
+  return round(returnRandomNumber()) / num;
+}
 
-  return `Seu número é ${Math.round(numberToDivide * 100) / num}!`;
+function operation(){
+  +: () operation['+'];
+  /: ()
 }
