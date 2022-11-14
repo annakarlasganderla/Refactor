d448ae6431f596d9575fe96ef8b120c58199c5a1
Rodrigo <rodrigo.facury14@gmail.com>
2022-11-14 12:17:31 -0300

    Revert "finalizado"
    This reverts commit 06ae7e75415112c81d3f75e103d15f823ef7e146.

--- refactor.js
+++ refactor.js
@@ -1,16 +1,31 @@
 function returnRandomNumber() {
-  let num  = Math.random();
-  return Math.round(num * 100)
+  return Math.random();
 }
+function round(num){
+  return `Seu número é ${Math.round(num * 100)}!`;
+}
+
+//Adicao
 function sumToRandomNumber(num) {
-return `Seu número é ${returnRandomNumber() + num}!`;
+  return round(returnRandomNumber()) + num;
 }
+
+//Subtracao
 function subtractRandomNumber(num) {
-return `Seu número é ${returnRandomNumber() - num}!`;
+  return round(returnRandomNumber()) - num;
 }
+
+//Multiplicacao
 function multiplyToRandomNumber(num) {
-return `Seu número é ${returnRandomNumber() * num}!`;
+  return round(returnRandomNumber()) * num;
 }
+
+//Divisao
 function divideRandomNumber(num) {
-return `Seu número é ${returnRandomNumber() / num}!`;
+  return round(returnRandomNumber()) / num;
+}
+
+function operation(){
+  +: () operation['+'];
+  /: ()
 }
