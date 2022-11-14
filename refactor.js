381ea419ffcaef8d5c8e89c3c41abe63b59aeb6c
anna sganderla <sganderlakarlaanna@gmail.com>
2022-11-14 19:27:01 -0300

    add refactor

--- refactor.js
+++ refactor.js
@@ -1,27 +1,21 @@
+const returnText = (response) => `Seu número é ${response}!`;
+
 function returnRandomNumber() {
-  return Math.random();
+  return Math.round(Math.random() * 100)
 }
 
-function sumToRandomNumber(num) {
-  const numberToSum = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToSum * 100) + num}!`;
+function sumToRandomNumber(number) {
+  return returnText(returnRandomNumber() + number);
 }
 
-function subtractRandomNumber(num) {
-  const numberToSubtract = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToSubtract * 100) - num}!`;
+function subtractRandomNumber(number) {
+  return returnText(returnRandomNumber() - number);
 }
 
-function multiplyToRandomNumber(num) {
-  const numberToMultiply = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToMultiply * 100) * num}!`;
+function multiplyToRandomNumber(number) {
+  return returnText(returnRandomNumber() * number);
 }
 
-function divideRandomNumber(num) {
-  const numberToDivide = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToDivide * 100) / num}!`;
+function divideRandomNumber(number) {
+  return returnText(returnRandomNumber() / number);
 }
